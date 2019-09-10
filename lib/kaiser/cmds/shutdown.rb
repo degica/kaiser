1f2471d0e5226844a97ac1de58b936dc834f3f50
Metallion <akieckens@axsh.net>
2019-09-10 17:12:13 +0900

    Added basic docs for Kaiser shutdown.

--- lib/kaiser/cmds/shutdown.rb
+++ lib/kaiser/cmds/shutdown.rb
@@ -3,13 +3,18 @@ module Kaiser
     class Shutdown < Cli
 
       def usage
-        "FORGOT TO ADD DOCUMENATION FOR SHUTDOWN. ADD IT!"
+      <<EOS
+Shuts down all the containers used internally by Kaiser.
+
+USAGE: kaiser shutdown
+EOS
       end
 
       def execute
         Config.config[:shared_names].each do |_, container_name|
           killrm container_name
         end
+
         CommandRunner.run Config.out, "docker network rm #{Config.config[:networkname]}"
         CommandRunner.run Config.out, "docker volume rm #{Config.config[:shared_names][:certs]}"
       end
