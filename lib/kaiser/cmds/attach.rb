ed64c299f0a63264c314128ee83c25eb7ed316b5
Metallion <akieckens@axsh.net>
2019-10-09 15:48:46 +0900

    Some quick documentation updates.

--- lib/kaiser/cmds/attach.rb
+++ lib/kaiser/cmds/attach.rb
@@ -7,6 +7,8 @@ module Kaiser
         <<~EOS
           Shuts down the application container and starts it up again with the current directory bind mounted inside. This way the application will run from the source code in the current directory and any edits you make will immediately show up inside the container. This is ideal for development.
 
+          Once the attached container exits (through the use of control+c for example) it will be replaced by a regular non-attached version of the app container.
+
           USAGE: kaiser attach
         EOS
       end
