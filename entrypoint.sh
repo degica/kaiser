50bd30a3cc3922545f7bd451f0b57099faad2bf4
David Siaw <dsiaw@degica.com>
2021-11-29 18:52:19 +0900

    update

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-COMMAND="kaiser $@" # Group up our command so we don't lose params
-cd "$CONTEXT_DIR"   # Move to the context directory
-sh -c "$COMMAND"    # Execute Kaiser
--- lib/kaiser/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Kaiser
-  VERSION = '0.5.2'
-end
