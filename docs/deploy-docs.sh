35198493ac7e658a1406b903afa8abd1adffe25d
David Siaw <dsiaw@degica.com>
2021-11-30 16:58:27 +0900

    gh workflow

---
+++ docs/deploy-docs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf build
+rm -rf gittemp
+
+git clone git@github.com:degica/kaiser gittemp
+pushd gittemp
+git checkout gh-pages
+popd
+weaver build --root "https://tech.degica.com/kaiser$ROOT_DIR"
+rm -rf build/js/MathJax
+echo "Making dir gittemp$ROOT_DIR"
+mkdir -p "gittemp$ROOT_DIR"
+cp -r build/* "gittemp$ROOT_DIR"
+pushd gittemp
+rm -rf js/MathJax
+git add .
+git add -u
+git commit -m "update"
+git push
+popd
