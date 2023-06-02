66069289def78bb342f53c58009a3780e21b229b
David Siaw <dsiaw@degica.com>
2023-06-02 12:03:05 +0900

    start up redis 7 (#87)
    * start up redis 7
    
    * bump version
    
    * fix reference

--- lib/kaiser/cli.rb
+++ lib/kaiser/cli.rb
@@ -534,7 +534,7 @@ module Kaiser
         "docker run -d
           --name #{Config.config[:shared_names][:redis]}
           --network #{Config.config[:networkname]}
-          redis:alpine"
+          redis:7-alpine"
       )
       run_if_dead(
         Config.config[:shared_names][:chrome],
